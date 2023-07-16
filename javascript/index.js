84d887ace97b3b06cd3142b9fae04f4c22ed1bc9
rutvalthonge4312 <127858102+rutvalthonge4312@users.noreply.github.com>
2023-07-16 13:30:16 +0530

    created upto groupClasses sections

--- javascript/index.js
+++ javascript/index.js
@@ -12,6 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
@@ -27,4 +28,36 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+}
+
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides1(n) {
+  showSlides1((slideIndex1 += n));
+}
+
+function currentSlide1(n) {
+  showSlides1((slideIndex1 = n));
+}
+
+function showSlides1(n) {
+  let i;
+
+  let slide = document.getElementsByClassName("slides");
+  let dot = document.getElementsByClassName("dot");
+  if (n > slide.length) {
+    slideIndex1 = 1;
+  }
+  if (n < 1) {
+    slideIndex1 = slide.length;
+  }
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+  slide[slideIndex1 - 1].style.display = "block";
+  dot[slideIndex1 - 1].className += " active";
 }
