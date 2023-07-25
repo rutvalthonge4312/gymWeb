523ecae624fa184b176693b89ef912ebe305ddc4
rutvalthonge4312 <127858102+rutvalthonge4312@users.noreply.github.com>
2023-07-25 21:58:41 +0530

    remaing the index page

--- javascript/signUpPage.js
+++ javascript/signUpPage.js
@@ -21,3 +21,33 @@
     );
   });
 })();
+var optionInput = document.getElementById("optionInput");
+
+var apiUrl = "http://localhost/GYM_API/index.php/user/getSubscription?limit=50";
+const request = new Request(apiUrl, {
+  method: "GET",
+});
+fetch(request)
+  .then((response) => response.json())
+  .then((responseJson) => {
+    console.log(JSON.stringify(responseJson));
+    if (responseJson.status == 200) {
+      console.log("hi Data Found!");
+
+      var i = 0;
+
+      responseJson.data.forEach((element) => {
+        let option = document.createElement("option");
+        option.setAttribute("value", element.name);
+
+        let optionText = document.createTextNode(element.name);
+        option.appendChild(optionText);
+
+        optionInput.appendChild(option);
+        i++;
+      });
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
